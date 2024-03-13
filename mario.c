dba075d9752ef50eeb81d97fefc62688d4c83c7c
Benson Njuguna <njugunab655@gmail.com>
2024-03-13 19:59:24 +0300

    08-do-while-loop

--- mario.c
+++ mario.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-  const int n = 4;
+  int n;
+
+  do {
+    printf("What's the block size? ");
+    scanf("%i", &n);
+  }
+  while (n < 1);
+
   for(int i = 0; i < n; i++) {
       for(int j = 0; j < n; j++) {
         printf("#");
