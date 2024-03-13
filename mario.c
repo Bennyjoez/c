c1408e4d7e6f181a16b16efdbef84ab9c859a232
Benson Njuguna <njugunab655@gmail.com>
2024-03-13 19:52:15 +0300

    07-nested-loops

--- mario.c
+++ mario.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main(void) {
-  for(int i = 0; i < 4; i++) {
-      for(int j = 0; j < 4; j++) {
+  const int n = 4;
+  for(int i = 0; i < n; i++) {
+      for(int j = 0; j < n; j++) {
         printf("#");
       }
     printf("\n");
